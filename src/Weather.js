d21bf57f90aab7da19f519fcca7a7bab9dc5d99f
Ann McGlone <92317108+inksp0ts09@users.noreply.github.com>
2022-05-21 16:23:11 -0400

    worked on formatting of icon and current weather in Weather and WeatherInfo

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,8 @@ export default function Weather(props) {
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
-      description: response.data.weather[0].icon,
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
       city: response.data.name
     });
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,16 +15,23 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <WeatherIcon code={props.data.icon} size={52} />
-
-          <WeatherTemperature celsius={props.data.temperature} />
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
+            <div className="float-left mb-2">
+              <WeatherTemperature
+                celsius={Math.round(props.data.temperature)}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="col-6">
-        <ul>
-          <li>Humidity: {props.data.humidity}%</li>
-          <li>Wind:{props.data.wind} km/hr</li>
-        </ul>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {Math.round(props.data.humidity)}%</li>
+            <li>Wind:{Math.round(props.data.wind)} km/hr</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
