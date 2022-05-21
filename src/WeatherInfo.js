7ff92728dffa19f456e9bfb517fce8f6eedc94e3
Ann McGlone <92317108+inksp0ts09@users.noreply.github.com>
2022-05-21 15:53:47 -0400

    Fixed issue with the Temperature " where there should have been }

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,21 +15,16 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
-            <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={52} />
-            </div>
-            <div className="float-left">
-              <WeatherTemperature celsius="{props.data.temperature" />
-            </div>
-          </div>
+          <WeatherIcon code={props.data.icon} size={52} />
+
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind:{props.data.wind} km/hr</li>
-          </ul>
-        </div>
+      </div>
+      <div className="col-6">
+        <ul>
+          <li>Humidity: {props.data.humidity}%</li>
+          <li>Wind:{props.data.wind} km/hr</li>
+        </ul>
       </div>
     </div>
   );
